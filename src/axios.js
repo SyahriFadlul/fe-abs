3d996078c4618123ad8232a2f8f395f5d9d7133e
syahri <syahri.f.muminin@gmail.com>
2024-09-22 22:51:21 +0700

    uncomment important things

--- src/axios.js
+++ src/axios.js
@@ -3,8 +3,8 @@ import { useAuthStore } from './stores/auth';
 import nProgress from 'nprogress';
 
 // axios.defaults.withCredentials = true
-// axios.defaults.withXSRFToken = true;
-// axios.defaults.headers.common['ngrok-skip-browser-warning'] = '1'; //ngrok API
+axios.defaults.withXSRFToken = true;
+axios.defaults.headers.common['ngrok-skip-browser-warning'] = '1'; //ngrok API
 axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL 
 
 
