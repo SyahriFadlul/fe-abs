763f2a4d8d176a84eb55b7e6d5bb5c63eaa62867
syahri <syahri.f.muminin@gmail.com>
2024-09-23 11:35:28 +0700

    add header to proxy

--- netlify/functions/proxy.js
+++ netlify/functions/proxy.js
@@ -4,7 +4,11 @@ export async function handler (event, context) {
   const imageUrl = 'https://cow-expert-plainly.ngrok-free.app' + event.path.replace('/api/proxy', '');
 
   try {
-    const response = await fetch(imageUrl);
+    const response = await fetch(imageUrl, {
+        headers:{
+            'ngrok-skip-browser-warning' : true
+        }
+    });
     const contentType = response.headers.get('content-type');
 
     if (!response.ok) {
